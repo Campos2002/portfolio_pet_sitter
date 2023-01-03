ae38ceaa6c6f1373eba3b88133d3554518e76c28
Campos2002 <campereira91@gmail.com>
2023-01-03 00:29:17 -0300

    modificações em portfolio, inclusão de icone no titulo

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './portfolio.css'
 import data from '../portfolio/data'
+import { MdPets } from 'react-icons/md'
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>Meus super satisfeitos</h5>
-      <h2>Clientinhos</h2>
+      <h2>Clientinhos <MdPets /></h2>
+
 
       <div className="container portfolio__container">
         {
-          data.map(({ image, title, github, demo }, index) => (
+          data.map(({ image, title }, index) => (
             <article key={index} className='portfolio__item'>
               <div className="portfolio__item-image">
                 <img src={image} alt={title} />
