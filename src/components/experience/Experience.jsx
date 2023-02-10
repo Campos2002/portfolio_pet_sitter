67678764d4ab1f4f5d53a155ca39edd43f446b0a
Campos2002 <campereira91@gmail.com>
2023-02-09 21:04:45 -0300

    modificação da seção experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './experience.css'
 import { BsPatchCheckFill } from 'react-icons/bs'
+import data from '../experience/data'
 
 const Experience = () => {
   return (
@@ -9,64 +10,27 @@ const Experience = () => {
       <h5>Especialidades</h5>
       <h2>Certificados</h2>
 
-      {/* Inicio do Frontend */}
       <div className="container experience__container">
 
-        <div className="experience__content">
+        <div div className="experience__content">
 
-          <artcile className='experience__details'>
-            <BsPatchCheckFill className='experience__details-icon' />
-            <div>
-              <h4>Análise Comportamental<br />de Felinos Domésticos</h4>
-              <small className='text-light'>Valéria Zukauskas - Gatos no Divã</small>
-            </div>
-          </artcile>
-
-          <article className='experience__details'>
-            <BsPatchCheckFill className='experience__details-icon' />
-            <div>
-              <h4>Pet Sitter e Dog Walker</h4>
-              <small className='text-light'>PetAnjo</small>
-            </div>
-          </article>
-
-          <article className='experience__details'>
-            <BsPatchCheckFill className='experience__details-icon' />
-            <div>
-              <h4>Auxiliar Veterinária</h4>
-              <small className='text-light'>Paixão Treinamentos</small>
-            </div>
-          </article>
-
-          <article className='experience__details'>
-            <BsPatchCheckFill className='experience__details-icon' />
-            <div>
-              <h4>Cat Sitter Profissional</h4>
-              <small className='text-light'>Cat Sitter de Sucesso</small>
-            </div>
-          </article>
-
-          <article className='experience__details'>
-            <BsPatchCheckFill className='experience__details-icon' />
-            <div>
-              <h4>Socorrista Pet</h4>
-              <small className='text-light'>Smart Centro de Ensino Pet</small>
-            </div>
-          </article>
-
-          <article className='experience__details'>
-            <BsPatchCheckFill className='experience__details-icon' />
-            <div>
-              <h4>Como Cuidar de Gatos</h4>
-              <small className='text-light'>Alexandre Rossi</small>
-            </div>
-          </article>
+          {
+            data.map(({ certificado, emissor }, index) => (
+              <artcile key={index} className='experience__details'>
+                <BsPatchCheckFill className='experience__details-icon' />
+                <div>
+                  <h4>{certificado}</h4>
+                  <small className='text-light'>{emissor}</small>
+                </div>
+              </artcile>
+            ))
+          }
 
         </div>
 
       </div>
 
-    </section>
+    </section >
   )
 }
 
---
+++ src/components/experience/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    certificado: 'Comportamentalista de Felinos',
+    emissor: 'Valéria Zukauskas-Gatos no Divã',
+  },
+  {
+    certificado: 'Pet Sitter e Dog Walker',
+    emissor: 'PetAnjo',
+  },
+  {
+    certificado: 'Auxiliar Veterinária',
+    emissor: 'Paixão Treinamentos',
+  },
+  {
+    certificado: 'Cat Sitter Profissional',
+    emissor: 'Cat Sitter de Sucesso',
+  },
+  {
+    certificado: 'Como Cuidar de Gatos',
+    emissor: 'Alexandre Rossi',
+  },
+  {
+    certificado: 'Socorrista Pet',
+    emissor: 'Smart Centro de Ensino Pet',
+  },
+];
+
+export default data;
