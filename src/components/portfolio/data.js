2b262471968180253e1a1212b2dc2267ee24670d
Campos2002 <campereira91@gmail.com>
2023-02-17 01:11:40 -0300

    inclusão de novos pets na seção portfolio

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -4,19 +4,13 @@ import IMG3 from '../../assets/clientes/tapioca.jpg'
 import IMG4 from '../../assets/clientes/maggie.jpg'
 import IMG5 from '../../assets/clientes/pandora.jpg'
 import IMG6 from '../../assets/clientes/ruivo.jpg'
+import IMG7 from '../../assets/clientes/ragnar.jpg'
+import IMG8 from '../../assets/clientes/meg.jpg'
 
 const data = [
   {
     image: IMG1,
     title: 'Arthur'
-  },
-  {
-    image: IMG5,
-    title: 'Pandora'
-  },
-  {
-    image: IMG6,
-    title: 'Ruivo'
   },
   {
     image: IMG2,
@@ -37,6 +31,14 @@ const data = [
   {
     image: IMG6,
     title: 'Ruivo'
+  },
+  {
+    image: IMG7,
+    title: 'Ragnar'
+  },
+  {
+    image: IMG8,
+    title: 'Meg'
   },
 ];
 
