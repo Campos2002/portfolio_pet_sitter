6a32e4e76d67f55acabf5ca776653dc0d31c899e
Campos2002 <campereira91@gmail.com>
2023-02-17 14:10:21 -0300

    inclusão da seção testimonials na navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './nav.css'
 import { AiOutlineHome } from 'react-icons/ai'
 import { AiOutlineUser } from 'react-icons/ai'
-import { BiBook } from 'react-icons/bi'
+import { BsPatchCheckFill } from 'react-icons/bs'
 import { BiMessageSquareDetail } from 'react-icons/bi'
 import { MdPets } from 'react-icons/md'
+import { BiBookHeart } from 'react-icons/bi'
 import { useState } from 'react'
 
 const Nav = () => {
@@ -15,8 +16,9 @@ const Nav = () => {
     <nav>
       <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsPatchCheckFill /></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><MdPets /></a>
+      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><BiBookHeart /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
     </nav>
   )
